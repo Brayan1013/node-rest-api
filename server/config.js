34b16a7e4b39cfdec5a67b91829d0ee013ff25a2
Brayan Mora <brayan@gmail.com>
2020-10-18 13:54:35 -0500

    Conection to mongo ATLAS

--- server/config.js
+++ server/config.js
@@ -12,10 +12,12 @@ process.env.SECRET = 'secret';
 
 ///checking the environment
 
-const env = process.env.NODE_ENV || 'production';
+let env = process.env.NODE_ENV || 'development';
 
-if (env === 'production') {
+if (env === 'development') {
   process.env.DATABASE = 'mongodb://localhost:2000/cafe';
+  console.log('WE ARE IN DEVELPMENT');
 } else {
   process.env.DATABASE = `mongodb+srv://brayan123:12345@cluster0.koskc.mongodb.net/cafe?retryWrites=true&w=majority`;
+  console.log('WE ARE IN PRODUCTION');
 }
